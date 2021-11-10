8fdb26d02d7795ac314aef1ad8f4d780edcc4858
Danteclave <jkokindiegame@gmail.com>
2021-11-10 17:19:14 +0100

    added model bois

---
+++ Models/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace byt_game.Models
+{
+    class Player : User
+    {
+        long RankingPoints { get; set; }
+
+
+        ICollection<Player> getFriends()
+        {
+            return friends;
+        }
+
+        void openMailbox()
+        {
+            ;
+        }
+
+        void InviteToTrade(Player other, Item item, int count)
+        {
+            ;
+        }
+
+        public ICollection<Player> friends;
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace byt_game.Models
+{
+    class User
+    {
+        string Username { get; set; }
+        string Password { get; set; }
+        string Email { get; set; }
+        DateTime Birthday_Date { get; set; }
+        string Contact_number { get; set; }
+    }
+}
