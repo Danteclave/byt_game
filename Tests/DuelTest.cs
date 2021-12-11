2d57fffdefad7238e78f3991098cacca35fbed62
LoadCake <stelod00@gmail.com>
2021-12-11 15:10:41 +0100

    finished the Duel Tests

---
+++ Tests/DuelTest.cs
@@ -0,0 +1,29 @@
+﻿using byt_game.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace byt_game.Tests
+{
+    public class DuelTest
+    {
+        [Fact]
+        public void DuelConstructorTest()
+        {
+            Duel duel = new Duel(new Player(), new Player(), true, 10);
+            Assert.NotNull(duel.HomePlayer);//if the constructor fails then this field would remain null
+        }
+
+        [Fact]
+        public void DuelConstrainsTest()
+        {
+            ICollection<ValidationResult> results = new List<ValidationResult>();
+            Duel duel = new Duel(new Player(), new Player(), true, 10);
+            Assert.True(Validator.TryValidateObject(duel, new ValidationContext(duel), results, true));//everything is set correctly so the validator should return true
+        }
+    }
+}
--- Tests/UserTest.cs
+++ Tests/UserTest.cs
@@ -18,7 +18,7 @@ namespace byt_game.Tests
             Player tmp = new Player(); //using Player because it inherits from the User and user is an abstract class
             tmp.Username = "12345678dfgshjhfd9012314";
             tmp.Password = "sosfydgfdshjkasdkfjhgshaukfigeu";
-            tmp.Email = "tfedaghudfsdusfdaujidegfyheaujgfajfgedajyhest@amogus.com";
+            tmp.Email = "tfedaghudfsdusfdaujidegfyheaujgfajfgedajyhest@gmail.com";
             tmp.Contact_number = "1234567895246732584612735";
             Assert.False(Validator.TryValidateObject(tmp, new ValidationContext(tmp), results, true));//checking if the validation fails because every field is set to an incorrect value.
         }
